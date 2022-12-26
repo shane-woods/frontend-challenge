d3b7e42c6b135460ede99a90eded20ec319af4b6
Shane <shwoods35@gmail.com>
2022-12-26 11:18:52 -0500

    restarted

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,1 +1,76 @@
-export {}
+import React, {useState, useEffect} from "react";
+import { API_URL } from "../../constants";
+import styles from "../../../styles/Feed.module.css"
+import Story from "../Story";
+
+type StoryProps = {
+  title: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+  thumbnail: {
+    type: string;
+    location: string;
+    raw: string;
+  }
+}
+
+const Feed: React.FC = () => {
+  const [stories, setStories] = useState<StoryProps[]>([]);
+  const [storiesLength, setStoriesLength] = useState(5);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getStories(length:number) {
+      setIsLoading(true);
+      const response = await fetch(API_URL, {
+        method: "GET"
+      });
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+  
+      const stories = await response.json();
+      setIsLoading(false);
+      setStories(stories.slice(0, storiesLength));
+  };
+
+  useEffect(() => {
+    getStories(storiesLength);
+  }, [storiesLength])
+
+  function handleClick() {
+    setStoriesLength(storiesLength + 5);
+    getStories(storiesLength);
+  }
+
+
+  return (
+    <>
+    {isLoading ? (
+      <div className={styles.loading}>
+        <button className={styles.loading}>Loading...</button>
+      </div>
+    ) : (
+      <div className={styles.feed}>
+      <div>
+        {
+          stories.map((story, i) => {
+            return (
+              <Story story={story} key={i}/>
+            );
+          })
+        }
+      </div>
+        <button className={styles.button} onClick={handleClick}>Load More</button>
+      </div>
+    )}
+    </>
+  )
+
+};
+
+export default Feed;
--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -1,1 +1,43 @@
-export {}
+import Image from "next/image";
+import React from "react";
+import styles from "../../../styles/Feed.module.css"
+
+type StoryProps = {
+  story: {
+    title: string;
+    author: {
+      name: string;
+      avatar: string;
+    }
+    thumbnail: {
+      type: string;
+      location: string;
+      raw: string;
+    }
+  }
+}
+
+const Story = ({story} : StoryProps) => {
+  return (
+    <>
+      <div className={styles.story}>
+          <div className={styles.thumbnail}>
+            <Image alt="Thumbnail for blog" src={story.thumbnail.raw} height="150" width="200"/>
+          </div>
+          <div className={styles.storyDetails}>
+            <p>{story.title}</p>  
+            <div className={styles.authorDetails}>
+              {story.author.avatar ? (
+                <Image className={styles.avatar} alt="Photo of author" src={story.author.avatar} height="30" width="30"/>
+              ): (
+                <div className="defaultAuthor"></div>
+              )}
+              <p>{story.author.name}</p>
+            </div>
+          </div>
+       </div>
+    </>
+  )
+};
+
+export default Story;
